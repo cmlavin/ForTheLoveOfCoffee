155770c21281f8a90107086427b1d7a2c5b7b364
Catherine <cmlavin7@gmail.com>
2017-10-09 19:04:35 -0400

    first commit

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+  return(
+    <div id="title-text">
+      For the Love of Coffee
+    </div>
+  )
+}
+
+export default Title
---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Title from '../Title'
+
+const Homepage = () => {
+  return(
+    <div>
+      <Navbar />
+      <Title />
+    </div>
+  )
+}
+
+export default Homepage
