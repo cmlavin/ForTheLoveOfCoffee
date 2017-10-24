5d2c1cd7f55e9c08aa2dd403c4d89db0d70a04cf
Catherine <cmlavin7@gmail.com>
2017-10-24 14:12:17 -0400

    Added header to login and signup pages

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,11 +3,9 @@ import React from 'react'
 const Welcome = () => {
   return(
     <div id="welcome-text">
-      Coffee shops these days are filled with people working away on their laptops. The right
-      atmosphere can make all the difference when it comes to your productivity and comfort.
-      But how do you know which ones are the best suited for your work style? Welcome to
-      For the Love of Coffee where you can read or write reviews of the best and worst coffee
-      shops for getting your work done.
+      Coffee shops are filled with people working away on their laptops. The right atmosphere
+      can make all the difference when it comes to your productivity and comfort. Join a
+      community where you can share reviews and discover new cafes in your area.
     </div>
   )
 }
