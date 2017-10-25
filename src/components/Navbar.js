16dd66e78d2532d07257c55e8e810e73b978da0b
Catherine <cmlavin7@gmail.com>
2017-10-25 12:42:07 -0400

    Minor edits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 const Navbar = () => {
   return(
     <div>
+      <Button className="button-navbar" basic>Write a Review</Button>
+      <Button className="button-navbar" basic>Login</Button>
+      <Button className="button-navbar" basic>Signup</Button>
     </div>
   )
 }
