a7461ca1af47e0a8872f74e95ce61431375474bd
Catherine <cmlavin7@gmail.com>
2017-12-16 14:36:20 -0500

    Adjusted positioning of navbar buttons and title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,20 @@ import { Button } from 'semantic-ui-react'
 
 const Navbar = () => {
   return(
-    <div>
-      <Link to="/writereview">
-        <Button basic>Write a Review</Button>
-      </Link>
-      <Link to="/login">
-        <Button basic>Login</Button>
-      </Link>
-      <Link to="/signup">
-        <Button basic>Signup</Button>
-      </Link>
+    <div className="navbar-container">
+      <div className="leftside-navbar">
+        <Link to="/writereview">
+          <Button basic>Write a Review</Button>
+        </Link>
+      </div>
+      <div className="rightside-navbar">
+        <Link to="/login">
+          <Button basic>Login</Button>
+        </Link>
+        <Link to="/signup">
+          <Button basic>Signup</Button>
+        </Link>
+      </div>
     </div>
   )
 }
